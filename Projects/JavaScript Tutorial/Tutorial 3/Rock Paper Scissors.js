a3888c4cf6f8087d06630a80c4f9a3f68ba34857
AlexSivoy <novsiv@gmail.com>
2022-10-08 15:52:12 +0200

    Rock Paper Scissors

---
+++ Projects/JavaScript Tutorial/Tutorial 3/Rock Paper Scissors.js
@@ -0,0 +1,61 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoise
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+
+    generateComputerChoice()
+    getResult()
+
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoise = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoise = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoise = 'paper'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoise
+}
+
+function getResult() {
+    if (computerChoise === userChoice) {
+        result = 'its a draw'
+    }
+
+    if (computerChoise === 'rock' && userChoice === 'paper') {
+        result = 'you win!'
+    }
+    if (computerChoise === 'rock' && userChoice === 'scissors') {
+        result = 'you lost!'
+    }
+    if (computerChoise === 'paper' && userChoice === 'rock') {
+        result = 'you lost!'
+    }
+    if (computerChoise === 'paper' && userChoice === 'scissors') {
+        result = 'you win!'
+    }
+    if (computerChoise === 'scissors' && userChoice === 'rock') {
+        result = 'you win!'
+    }
+    if (computerChoise === 'scissors' && userChoice === 'paper') {
+        result = 'you lost!'
+    }
+
+    resultDisplay.innerHTML = result
+
+}
---
+++ Projects/JavaScript Tutorial/Tutorial 3/readme.txt
@@ -0,0 +1,2 @@
+to start - copy html path and open it in chrome 
+tutorial: https://www.youtube.com/watch?v=ec8vSKJuZTk&t=287s
