a5e81cfdb76f51e736152be749d714d745deef32
AlexSivoy <novsiv@gmail.com>
2022-10-22 18:25:08 +0200

    frogger v4

--- Projects/JavaScript Tutorial/Tutorial 3/Frogger/app.js
+++ Projects/JavaScript Tutorial/Tutorial 3/Frogger/app.js
@@ -4,12 +4,17 @@ const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
+
 
 console.log(squares)
 let currentIndex = 76
 const width = 9
+let timerId
 
-//frog moving logic
+//frog moving logic:
 function moveFrog(e) {
     squares[currentIndex].classList.remove('frog') //removes html class 'frog' from selected square 
 
@@ -41,9 +46,13 @@ function moveFrog(e) {
 
 document.addEventListener('keyup', moveFrog)
 
-//log moving logic
-function autoMoveLogs() {
+//moving logic for blocks:
+function autoMoveElements() {
     logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    logsRight.forEach(logRight => moveLogRight(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
+    lose()
 }
 
 function moveLogLeft(logLeft) {
@@ -52,7 +61,92 @@ function moveLogLeft(logLeft) {
             logLeft.classList.remove('l1')
             logLeft.classList.add('l2')
             break
+        case logLeft.classList.contains('l2'):
+            logLeft.classList.remove('l2')
+            logLeft.classList.add('l3')
+            break
+        case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
+            break
+        case logLeft.classList.contains('l4'):
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
+            break
+        case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
+            break
     }
 }
 
-setInterval(autoMoveLogs, 1000)
+function moveLogRight(logRight) {
+    switch (true) {
+        case logRight.classList.contains('l1'):
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+        case logRight.classList.contains('l2'):
+            logRight.classList.remove('l2')
+            logRight.classList.add('l1')
+            break
+        case logRight.classList.contains('l3'):
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break
+        case logRight.classList.contains('l4'):
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break
+        case logRight.classList.contains('l5'):
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
+            break
+    }
+}
+
+function moveCarLeft(carLeft) {
+    switch (true) {
+        case carLeft.classList.contains('c1'):
+            carLeft.classList.remove('c1')
+            carLeft.classList.add('c2')
+            break
+        case carLeft.classList.contains('c2'):
+            carLeft.classList.remove('c2')
+            carLeft.classList.add('c3')
+            break
+        case carLeft.classList.contains('c3'):
+            carLeft.classList.remove('c3')
+            carLeft.classList.add('c1')
+            break
+    }
+}
+
+function moveCarRight(carRight) {
+    switch (true) {
+        case carRight.classList.contains('c1'):
+            carRight.classList.remove('c1')
+            carRight.classList.add('c3')
+            break
+        case carRight.classList.contains('c2'):
+            carRight.classList.remove('c2')
+            carRight.classList.add('c1')
+            break
+        case carRight.classList.contains('c3'):
+            carRight.classList.remove('c3')
+            carRight.classList.add('c2')
+            break
+    }
+}
+
+// Lose logic:
+function lose() {
+    if (squares[currentIndex].classList.contains('c1')) {
+        resultDisplay.textContent = 'You Lose!'
+        clearInterval(timerId)
+        squares[currentIndex].classList.remove('frog')
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+timerId = setInterval(autoMoveElements, 1000)
--- Projects/JavaScript Tutorial/Tutorial 3/Frogger/readme.txt
+++ Projects/JavaScript Tutorial/Tutorial 3/Frogger/readme.txt
@@ -1,2 +1,2 @@
 https://www.youtube.com/watch?v=ec8vSKJuZTk&t=5570s
-3.14
+3.27
