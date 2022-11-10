f93ab3af678b127c98a488c898d6e284054e35c9
AlexSivoy <novsiv@gmail.com>
2022-11-10 21:24:31 +0100

    Coolours v1

--- Projects/JavaScript Tutorial/Coolours/README.txt
+++ Projects/JavaScript Tutorial/Coolours/README.txt
@@ -1,4 +1,4 @@
 Tutorial:
 https://www.youtube.com/watch?v=OaNICHKM5KM&list=WL&index=5&t=72s
 
-18:41
+30.50
--- Projects/JavaScript Tutorial/Coolours/app.js
+++ Projects/JavaScript Tutorial/Coolours/app.js
@@ -1,19 +1,50 @@
 const cols = document.querySelectorAll('.col')
 
-function generateRandomcolor() {
-    const hexColors = '123456789ABCDEF'
-    let color = ''
-    for (let i = 0; i < 6; i++) {
-        let random = Math.floor(Math.random() * hexColors.length)
-        color += hexColors[random]
+document.addEventListener('keydown', event => {
+    if (event.code.toLocaleLowerCase() === 'space') {
+        setRandomColors()
     }
-    return '#' + color
-}
+})
+
+document.addEventListener('click', event => {
+    const type = event.target.dataset.type
+    if (type === 'lock') {
+        console.log('perform lock')
+    }
+})
+
+//function is not used, has been used as example only 
+// function generateRandomcolor() {
+//     const hexColors = '123456789ABCDEF'
+//     let color = ''
+//     for (let i = 0; i < 6; i++) {
+//         let random = Math.floor(Math.random() * hexColors.length)
+//         color += hexColors[random]
+//     }
+//     return '#' + color
+// }
 
 function setRandomColors() {
     cols.forEach(col => {
-        col.style.background = generateRandomcolor()
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+
+        //const color = generateRandomcolor()
+        const color = chroma.random()
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(button, color)
+        setTextColor(text, color)
     });
+}
+
+//changes element color accordingly to backround color
+//input: html element, color
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
