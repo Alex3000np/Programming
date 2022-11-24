25362e827657bad855cc017bf9bae6e9f0d2a3fa
AlexSivoy <novsiv@gmail.com>
2022-11-24 21:14:49 +0100

    Coolours v1

--- Projects/JavaScript Tutorial/Coolours/app.js
+++ Projects/JavaScript Tutorial/Coolours/app.js
@@ -1,15 +1,25 @@
 const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', event => {
+    event.preventDefault()
     if (event.code.toLocaleLowerCase() === 'space') {
         setRandomColors()
     }
 })
 
 document.addEventListener('click', event => {
+    
     const type = event.target.dataset.type
     if (type === 'lock') {
-        console.log('perform lock')
+        //check if clicked item's Tagname is "i", in other case it's "button" around icon
+        const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target              //if "i" pressed -> we take <i> </i> element from HTML
+            : event.target.children[0]  //if "button" pressed -> we take first item inside "button" (which is same <i>...</i> element from HTML)
+        //from this moment 'node' is <i> </i> element from HTML
+
+        //switches class name
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
     }
 })
 
