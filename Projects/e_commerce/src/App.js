71c78f0fddc6be9259d4c8562e555625fca100be
AlexSivoy <novsiv@gmail.com>
2022-12-10 20:05:35 +0100

    adds images and description

--- Projects/e_commerce/src/App.js
+++ Projects/e_commerce/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Products from './components/Products/Products';
 
 const App = () => {
   return (
-    <div>E-commerce</div>
+    <div>
+      <Products />
+    </div>
   )
 }
 
---
+++ Projects/e_commerce/src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+
+const products = [
+    { id: 1, name: "shoes", description: "running shoes", price: "$5", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Adidas_Conductor_High_Olympics_1988_re-edition_sneakers.jpg/640px-Adidas_Conductor_High_Olympics_1988_re-edition_sneakers.jpg" },
+    { id: 2, name: "macbook", description: "apple macbook", price: "$10", image: "https://upload.wikimedia.org/wikipedia/commons/1/11/Macbook_white_redjar_20060603.jpg" },
+
+];
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+
+                ))}
+
+            </Grid>
+        </main>
+    );
+}
+
+export default Products;
