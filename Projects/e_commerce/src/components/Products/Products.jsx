119a8992961156756612c7b9a8aadecb93f1e209
AlexSivoy <novsiv@gmail.com>
2022-12-11 16:13:11 +0100

    adds navigation bar

--- Projects/e_commerce/src/components/Products/Products.jsx
+++ Projects/e_commerce/src/components/Products/Products.jsx
@@ -3,6 +3,8 @@ import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
 
+import useStyles from "./styles";
+
 const products = [
     { id: 1, name: "shoes", description: "running shoes", price: "$5", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Adidas_Conductor_High_Olympics_1988_re-edition_sneakers.jpg/640px-Adidas_Conductor_High_Olympics_1988_re-edition_sneakers.jpg" },
     { id: 2, name: "macbook", description: "apple macbook", price: "$10", image: "https://upload.wikimedia.org/wikipedia/commons/1/11/Macbook_white_redjar_20060603.jpg" },
@@ -10,8 +12,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
